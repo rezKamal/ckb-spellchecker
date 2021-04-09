11937a49c601d8dcb5a24ab9d55c30043bc3ec22
rkamal1 <rkamal1@swarthmore.edu>
2021-04-08 20:16:53 -0400

    Update disambiguation-test.sh

--- tests/disambiguation-test.sh
+++ tests/disambiguation-test.sh
@@ -1,9 +1,9 @@
 CORPUS=../ling073-ckb-corpus/ckb.wikipedia.2021-02-23.txt
-MORPH=../ckb.automorf.bin
-RLX=../ckb.rlx.bin # you might need to remove ".bin"
-TOKENS=`cat ${CORPUS} | lt-proc ${MORPH} | sed 's/$\W*\^/$\n^/g' | wc -l`
-ANALYSES=`cat ${CORPUS} | lt-proc ${MORPH} | sed 's/$\W*\^/$\n^/g' | cut -f2- -d'/' | sed 's/\//\n/g' | wc -l`
-DISAMB=`cat ${CORPUS} | lt-proc ${MORPH} | cg-proc ${RLX} | sed 's/$\W*\^/$\n^/g' | cut -f2- -d'/' | sed 's/\//\n/g' | wc -l`
+MORPH=../ckb.automorf.hfst
+RLX=../apertium-ckb.ckb.rlx # you might need to remove ".bin"
+TOKENS=`cat ${CORPUS} | hfst-proc ${MORPH} | sed 's/$\W*\^/$\n^/g' | wc -l`
+ANALYSES=`cat ${CORPUS} | hfst-proc ${MORPH} | sed 's/$\W*\^/$\n^/g' | cut -f2- -d'/' | sed 's/\//\n/g' | wc -l`
+DISAMB=`cat ${CORPUS} | hfst-proc ${MORPH} | cg-proc ${RLX} | sed 's/$\W*\^/$\n^/g' | cut -f2- -d'/' | sed 's/\//\n/g' | wc -l`
 AMBIGPRE=`calc $ANALYSES/$TOKENS`
 AMBIGPOST=`calc $DISAMB/$TOKENS`
 echo "Ambiguity before disambiguation: ${AMBIGPRE}"
